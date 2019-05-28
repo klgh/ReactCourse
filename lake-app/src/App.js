b56525c64da7291ec76d056c7f42c1a74e554ab0
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2019-05-28 17:53:31 -0400

    done

--- lake-app/src/App.js
+++ lake-app/src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-
 
 const App = ({ lakes }) => (
   <ul>
--- lake-app/src/index.js
+++ lake-app/src/index.js
@@ -5,9 +5,9 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 const lakeList = [
-    {id: 1, name: "Echo Lake", trailhead: "Echo Lake"}, 
-    {id: 2, name: "Maud Lake", trailhead: "Wright's Lake"}, 
-    {id: 3, name: "Cascade Lake", trailhead: "Bayview"}
+  { id: 1, name: "Echo Lake", trailhead: "Echo Lake" },
+  { id: 2, name: "Maud Lake", trailhead: "Wright's Lake" },
+  { id: 3, name: "Cascade Lake", trailhead: "Bayview" }
 ];
 
 ReactDOM.render(<App lakes={lakeList} />, document.getElementById("root"));
