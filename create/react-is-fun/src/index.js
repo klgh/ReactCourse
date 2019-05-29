3c460eeb1369e9b6661c22e7bcb0210b2255d495
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2019-05-29 12:06:52 -0400

    components

--- create/react-is-fun/src/index.js
+++ create/react-is-fun/src/index.js
@@ -1,46 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { render } from "react-dom";
 
-let skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 100
-};
-
-const getPercent = decimal => {
-  return decimal * 100 + "%";
-};
-
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total / goal);
-};
-
-const SkiDayCounter = ({ total, powder, backcountry, goal }) => {
+const Book = ({ title, author, pages }) => {
   return (
     <section>
-      <div>
-        <p>Total Days: {total}</p>
-      </div>
-      <div>
-        <p>Powder Days: {powder}</p>
-      </div>
-      <div>
-        <p>Backcountry Days: {backcountry}</p>
-      </div>
-      <div>
-        <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
-      </div>
+      <h2>{title}</h2>
+      <p>By: {author}</p>
+      <p>pages: {pages} pages</p>
     </section>
   );
 };
 
+const Library = () => {
+  return (
+    <div>
+      <Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260} />
+      <Book title="White Teeth" author="Zandie Smith" pages={580} />
+      <Book title="Cat's Cradel" author="Kurt Vonn" pages={3607} />
+    </div>
+  );
+};
+
 render(
-  <SkiDayCounter
-    total={skiData.total}
-    powder={skiData.powder}
-    backcountry={skiData.backcountry}
-    goal={skiData.goal}
-  />,
-  document.getElementById("root")
+    <Library />, 
+    document.getElementById("root")
 );
