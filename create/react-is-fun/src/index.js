fc889f2f80ac226df77308556e7d9e84b2a1b5c0
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2019-05-29 11:54:47 -0400

    udpate

--- create/react-is-fun/src/index.js
+++ create/react-is-fun/src/index.js
@@ -8,27 +8,32 @@ let skiData = {
   goal: 100
 };
 
-class SkiDayCounter extends Component {
-  render() {
-      const {total, powder, backcountry, goal} = this.props
-    return (
-      <section>
-        <div>
-          <p>Total Days: {total}</p>
-        </div>
-        <div>
-          <p>Poweder Days: {powder}</p>
-        </div>
-        <div>
-          <p>Backcountry Days: {backcountry}</p>
-        </div>
-        <div>
-          <p>Goal: {goal}</p>
-        </div>
-      </section>
-    );
-  }
-}
+const getPercent = decimal => {
+  return decimal * 100 + "%";
+};
+
+const calcGoalProgress = (total, goal) => {
+  return getPercent(total / goal);
+};
+
+const SkiDayCounter = ({ total, powder, backcountry, goal }) => {
+  return (
+    <section>
+      <div>
+        <p>Total Days: {total}</p>
+      </div>
+      <div>
+        <p>Powder Days: {powder}</p>
+      </div>
+      <div>
+        <p>Backcountry Days: {backcountry}</p>
+      </div>
+      <div>
+        <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
+      </div>
+    </section>
+  );
+};
 
 render(
   <SkiDayCounter
