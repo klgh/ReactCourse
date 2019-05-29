f2bce7f6250207f77d74cf6b0eaa156d9e79e813
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2019-05-29 13:15:16 -0400

    states

--- create/react-is-fun/src/index.js
+++ create/react-is-fun/src/index.js
@@ -1,27 +1,68 @@
 import React from "react";
 import { render } from "react-dom";
 
-const Book = ({ title, author, pages }) => {
+let bookList = [
+  { title: "Hunger", author: "Roxane Gay", pages: 320 },
+  { title: "The Sun Also Rises", author: "Ernest Hemingway", pages: 260 },
+  { title: "White Teeth", author: "Zadie Smith", pages: 480 },
+  { title: "Cat's Cradle", author: "Kurt Vonnegut", pages: 304 }
+];
+
+const Book = ({ title, author, pages, freeBookmark }) => {
   return (
     <section>
       <h2>{title}</h2>
-      <p>By: {author}</p>
-      <p>pages: {pages} pages</p>
+      <p>by: {author}</p>
+      <p>Pages: {pages} pages</p>
+      <p>Free Bookmark Today: {freeBookmark ? "yes" : "no!"}</p>
     </section>
   );
 };
 
-const Library = () => {
-  return (
-    <div>
-      <Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260} />
-      <Book title="White Teeth" author="Zandie Smith" pages={580} />
-      <Book title="Cat's Cradel" author="Kurt Vonn" pages={3607} />
-    </div>
-  );
-};
-
-render(
-    <Library />, 
-    document.getElementById("root")
+const Hiring = () => (
+  <div>
+    <p>The Library is hiring! come on in</p>
+  </div>
 );
+
+const NotHiring = () => (
+  <div>
+    <p>The Library is not hiring! check back later</p>
+  </div>
+);
+
+class Library extends React.Component {
+  state = {
+    open: true,
+    freeBookmark: true,
+    hiring: true
+  };
+
+  toggleOpenClosed = () => {
+    this.setState(prevState => ({
+      open: !prevState.open
+    }));
+  };
+
+  render() {
+    const { books } = this.props;
+    return (
+      <div>
+        {this.state.hiring ? <Hiring /> : <NotHiring />}
+        <h1>The library is {this.state.open ? "open" : "closed"}</h1>
+        <button onClick={this.toggleOpenClosed}>Change</button>
+        {books.map((book, i) => (
+          <Book
+            key={i}
+            title={book.title}
+            author={book.author}
+            pages={book.pages}
+            freeBookmark={this.state.freeBookmark}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+render(<Library books={bookList} />, document.getElementById("root"));
